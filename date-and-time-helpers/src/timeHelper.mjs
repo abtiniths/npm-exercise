0a292b05449992acb48a2f9ffb3228fc73be5f95
abtiniths <abtin.nouran@iths.se>
2022-11-15 11:02:46 +0100

    changed npm package published minor

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -5,11 +5,19 @@ dayjs.locale("en-gb"); // en-gb starts week on monday, en does not
 const yyyymmdd = "YYYY-MM-DD"; // useful for simple .format(yyyymmdd)
 
 const timeHelper = ({ startDate, endDate }) => {
-  const mvalue = dayjs(startDate, endDate).format("MMMM, YYYY");
-  const yvalue = dayjs(endDate).format("MMMM, YYYY");
-  return `${mvalue}  ${yvalue};`;
+  if (
+    dayjs(startDate).date() !== 1 ||
+    dayjs(startDate).month() !== dayjs(endDate).month() ||
+    dayjs(startDate).endOf("month").date() !== dayjs(endDate).date()
+  ) {
+    return "no valid period";
+  } else {
+    return dayjs(startDate).format("MMMM");
+  }
 };
 
-console.log(timeHelper({ startDate: "2021-08-01", endDate: "2033-11-31" }));
-console.log(timeHelper({ startDate: "2021-09-01", endDate: "20241-09-31" }));
-console.log(timeHelper({ startDate: "2021-10-01", endDate: "2031-10-31" }));
+console.log(timeHelper({ startDate: "2021-08-01", endDate: "2033-08-31" }));
+console.log(timeHelper({ startDate: "2021-02-01", endDate: "20241-02-28" }));
+console.log(timeHelper({ startDate: "2021-10-01", endDate: "2031-12-31" }));
+
+export default timeHelper;
